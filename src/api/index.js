fad6b54089e7761ec43318588862ee1acf8c4242
rkpattnaik780 <rkpattnaik780@gmail.com>
2019-07-15 11:16:32 +0530

    Chore: lay proper directory structure for vue app

---
+++ src/api/index.js
@@ -0,0 +1,2 @@
+import axios from "axios";
+import { API_ROOT } from "@/config";
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+import "@babel/polyfill";
 import Vue from "vue";
+import "./plugins/vuetify";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import router from "@/router";
+import store from "@/store";
+import "@/assets/sass/main.scss";
 
 Vue.config.productionTip = false;
 
